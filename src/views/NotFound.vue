3232c53458b01d7bdb771164792a3261d852ac5d
_ <pedropmancillac@gmail.com>
2022-06-18 21:50:09 -0400

    not found

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,44 +3,20 @@
     <h1 v-text="`404`" />
     <h2 v-text="`Not Found`" />
     <div class="composition">
-      <img
-        id="sombra"
-        class="gatito"
-        src="@/assets/Cute-funny-cat-astronaut-in-space.png"
-        alt="Not found image"
-      />
-      <img id="fondo" src="@/assets/space.jpg" alt="Not found image" />
-      <img
-        id="gatito"
-        src="@/assets/Cute-funny-cat-astronaut-in-space.png"
-        alt="Not found image"
-      />
+      <img id="sombra" src="@/assets/Cute-funny-cat-astronaut-in-space.png" />
+      <img id="fondo" src="@/assets/space.jpg" />
+      <img id="gatito" src="@/assets/Cute-funny-cat-astronaut-in-space.png" />
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "NotFound",
-  // props: {},
-  // data(){}
-  // computed: {},
-  // methods: {}
-  // watch: {},
-  // components: {},
-  // mixins: [],
-  // filters: {},
-  // -- Lifecycle Methods
-  // -- End Lifecycle Methods
-};
+export default { name: "NotFound" };
 </script>
 
 <style lang="scss" scoped>
 @keyframes flotar {
   0% {
-    transform: translatey(0px);
-  }
-  50% {
     transform: translatey(20px);
   }
   100% {
@@ -49,16 +25,12 @@ export default {
 }
 @keyframes sombraFlotar {
   0% {
-    transform: translatey(0px);
-  }
-  50% {
     transform: translatey(-3px) translatex(15px);
   }
   100% {
     transform: translatey(0px);
   }
 }
-
 #NotFound {
   text-align: center;
   .composition {
@@ -71,23 +43,22 @@ export default {
       margin: auto;
       height: 50vh;
     }
-  }
-  #gatito {
-    animation: flotar 8s ease-in-out infinite;
-  }
-  #sombra {
-    left: -40px;
-    top: 25px;
-    filter: grayscale(100%) brightness(0%);
-    animation: sombraFlotar 8s ease-in-out infinite;
-    transform: translatey(36px);
-  }
-  #fondo {
-    border-radius: 50%;
-    width: 50vh;
-    opacity: 0.85;
-    box-shadow: -10px -8px 8px black;
-    transform: rotate(-95deg);
+    #fondo {
+      border-radius: 50%;
+      width: 50vh;
+      opacity: 0.85;
+      box-shadow: -10px -8px 8px black;
+      transform: rotate(-95deg);
+    }
+    #gatito {
+      animation: flotar 5s ease-in-out infinite alternate-reverse;
+    }
+    #sombra {
+      left: -40px;
+      top: 25px;
+      filter: grayscale(100%) brightness(0%);
+      animation: sombraFlotar 5s ease-in-out infinite alternate-reverse;
+    }
   }
 }
 </style>
